ba28287ec0be48daa27b05d5932a1c42ee2246ca
Yang Zou <yaz128@ucsd.edu>
2026-04-26 16:52:29 -0700

    fix bug

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,10 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
-
-        result = num1 + num2;
-
-        return result;
+        return num1 + num2
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -25,11 +21,12 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (prices.length === 0) {
+        return false;
+    }
     const discounted = []
-    const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+    for (let i = 0; i < prices.length; i++) {
+        let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
